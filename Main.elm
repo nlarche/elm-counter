330fdd541815ae47d0baa3cc8f255996466c0df4
nilar <nicolas.larche@rca.fr>
2016-07-20 15:38:06 +0200

    initial commit

---
+++ Main.elm
@@ -0,0 +1,77 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.App as App
+
+
+-- model
+
+
+type alias Model =
+    Int
+
+
+initModel : Model
+initModel =
+    0
+
+
+
+-- update
+
+
+type Msg
+    = AddCalorie
+    | RemoveCalorie
+    | Clear
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddCalorie ->
+            model + 1
+
+        RemoveCalorie ->
+            model - 1
+
+        Clear ->
+            initModel
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h3 []
+            [ text (("Total ") ++ (toString model)) ]
+        , button
+            [ type' "button"
+            , onClick AddCalorie
+            ]
+            [ text "Add" ]
+        , button
+            [ type' "button"
+            , onClick RemoveCalorie
+            ]
+            [ text "Remove" ]
+        , button
+            [ type' "button"
+            , onClick Clear
+            ]
+            [ text "Clear" ]
+        ]
+
+
+main : Program Never
+main =
+    App.beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        }
